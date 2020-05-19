7849f0bdc1971f8dd2c17310a522e7d5df9265ea
Chris Allen <chrisallen415@gmail.com>
2020-05-19 16:26:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,10 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
       @name = name
+      @breed = breed
+      if !breed
+        @breed = "Mutt"
+      end
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
 end
