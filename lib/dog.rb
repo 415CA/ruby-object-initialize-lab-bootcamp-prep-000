c2064ed69507024d4696ed3146a8ea252daf9342
Chris Allen <chrisallen415@gmail.com>
2020-05-19 16:20:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,14 @@ class Dog
     if breed = nil
       @breed = "Mutt"
     end
-    
+
     @breed = breed
   end
- 
+
   def breed=(breed)
     @breed = breed
   end
- 
+
   def breed
     @breed
   end
